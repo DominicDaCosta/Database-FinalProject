75f1fdffc0a99f749cb4e70363fe418945b34e75
Quyen Le <quyendle88@gmail.com>
2024-04-29 03:10:26 -0400

    completed query functions

--- request-db.php
+++ request-db.php
@@ -3,7 +3,7 @@
 function getMovieInfo()
 {
     global $db;
-    $query = "SELECT title, releaseYear FROM movie";
+    $query = "SELECT title, releaseYear, runtime, avgRating FROM movie natural join imdb_info";
     
     $statement = $db->prepare($query);
     $statement->execute();
@@ -16,7 +16,7 @@ function getMovieInfo()
 function getMoviebyTitle()
 {
     global $db;
-    $query = "SELECT title, releaseYear FROM movie where title like :searchTerm";
+    $query = "SELECT title, releaseYear, runtime, avgRating FROM movie natural join imdb_info where title like :searchTerm";
     $searchTerm = '%' . $searchTerm . '%';
     
     $statement = $db->prepare($query);
@@ -29,6 +29,96 @@ function getMoviebyTitle()
     return $result;
 }
 
+function getCrewbyMovieID($movieID) {
+    global $db;
+    $query = "select crewID, cname from crew natural join works_for where movieID=:movieID";
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue('movieID', $movieID);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $result;
+}
+
+function getJobsbyCrewID($crewID, $movieID) {
+    global $db;
+    $query = "select jobTitle from crew_job natural join works_for where crewID=:crewID and movieID=:movieID";
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue('crewID', $crewID);
+    $statement->bindValue('movieID', $movieID);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $result;
+}
+
+function getReviewbyUserID($userID)
+{
+    global $db;
+    $query = "select title, rating, content from review natural join movie where userID=:userID";
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':userID', $userID);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $result;
+}
+
+function getReviewbyMovieID($movieID)
+{
+    global $db;
+    $query = "select title, rating, content from review natural join movie where movieID=:movieID";
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':movieID', $movieID);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $result;
+}
+
+function getWatchlistbyUserID($userID)
+{
+    global $db;
+    $query = "select ltitle, list_desc from watchlist where userID=:userID";
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':userID', $userID);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $result;
+}
+
+function getMoviesbyListID($listID)
+{
+    global $db;
+    $query = 'select title, releaseYear, avgRating from is_added natural join movie natural join imdb_info where listID=:listID';
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':listID', $listID);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $result;
+}
+
+// CHECK ON THIS ONE
 function addReview($userID, $movieID, $rating, $content)
 {
     global db;
@@ -49,14 +139,16 @@ function addReview($userID, $movieID, $rating, $content)
     }
 }
 
-function addWatchlist($ltitle)
+function addWatchlist($ltitle, $userID)
 {
     global $db;
-    $query = "INSERT INTO watchlist (ltitle) VALUES (:ltitle)";
+    $query = "INSERT INTO watchlist (ltitle, list_desc, userID) VALUES (:ltitle, :list_desc, :userID)";
 
     try {
         $statement = $db->prepare($query)
         $statement->bindValue(':ltitle', $ltitle);
+        $statement->bindValue(':list_desc', $list_desc);
+        $statement->bindValue(':userID', $userID);
 
         $statement->execute();
         $statement->closeCursor();
@@ -66,6 +158,26 @@ function addWatchlist($ltitle)
     }
 }
 
+function addMovie($movieID, $listID, $has_watched)
+{
+    global $db;
+    $query = "INSERT INTO is_added (movieID, listID, has_watched) VALUES (:movieID, :listID, :has_watched)";
+
+    try {
+        $statement = $db->prepare($query)
+        $statement->bindValue(':movieID', $movieID);
+        $statement->bindValue(':listID', $movieID);
+        $statement->bindValue(':has_watched', $has_watched);
+
+        $statement->execute();
+        $statement->closeCursor();
+    } catch (PDOException $e)
+    {
+        $e->getMessage();
+    }
+}
+
+// CHECK ON THIS ONE
 function updateReview($userID, $movieID, $rating, $content)
 {
     global $db;
@@ -108,6 +220,7 @@ function deleteWatchlist($listID)
     $statement->closeCursor();
 }
 
+// UNTESTED
 function deleteReview($userID, $movieID)
 {
     global $db;
@@ -132,7 +245,7 @@ function deleteMovie($movieID, $listID)
     $statement->closeCursor();
 }
 
-function getMovieByGenre($gname)
+function filterMovieByGenre($gname)
 {
     global db;
     $query = "select title, releaseYear from movie natural join movie_genre where gname=:gname";
@@ -146,10 +259,10 @@ function getMovieByGenre($gname)
     return $result;
 }
 
-function getMovieByRating($avgRating) 
+function filterMovieByRating($avgRating) 
 {
     global db;
-    $query = "select title, releaseYear from movie where avgRating > :avgRating";
+    $query = "select title, releaseYear, runtime, avgRating from movie natural join imdb_info where avgRating > :avgRating";
     
     $statement = $db->prepare($query)
     $statement->bindValue(':avgRating', $avgRating);
